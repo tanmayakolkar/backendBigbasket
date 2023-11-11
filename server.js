d86ff1bd15fb9a8f0a8257f74e02234a1b352c89
tanmay akolkar <mail.tony24@gmail.com>
2023-11-10 20:15:45 -0800

    add new module called cors

--- server.js
+++ server.js
@@ -2,9 +2,11 @@ import express from "express";
 import mongoose from "mongoose";
 import mongodburl from "./config/dbConfig.js";
 import productRoute from "./router/productRoutes.js";
+import cors from 'cors'
 
 var app = express();
 app.use(express.json());
+app.use(cors())
 app.use('/api',productRoute);
 
 mongoose.connect(mongodburl).then(() => {
